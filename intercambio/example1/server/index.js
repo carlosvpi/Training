44db081431709569865a1cf6874b58cb7a49bd1b
CarlosVazPI <carlos.vazpi@gmail.com>
2021-10-24 20:00:00 +0200

    Added second example

--- intercambio/example1/server/index.js
+++ intercambio/example1/server/index.js
@@ -2,21 +2,14 @@
 
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
 
 app.use('/', express.static(`${__dirname}/../public`))
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
 app.engine('pug', require('pug').__express) // https://stackoverflow.com/questions/45342307/error-cannot-find-module-pug
 app.set('views', './views')
 app.set('view engine', 'pug')
 
 app.get('/pug', (req, res) => {
 	res.render(`index.pug`, { message: req.query.info || 'No info' })
-})
-
-app.get('/get/:item', (req, res) => {
-  res.send(`Here you have "${req.params.item}", with query = ${JSON.stringify(req.query)}`)
 })
 
 const port = 3000
--- intercambio/example2/server/index.js
+++ intercambio/example2/server/index.js
@@ -4,9 +4,9 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 
-app.use('/', express.static(`${__dirname}/../public`))
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(`${__dirname}/../public`))
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 app.engine('pug', require('pug').__express) // https://stackoverflow.com/questions/45342307/error-cannot-find-module-pug
 app.set('views', './views')
 app.set('view engine', 'pug')
@@ -15,8 +15,12 @@ app.get('/pug', (req, res) => {
 	res.render(`index.pug`, { message: req.query.info || 'No info' })
 })
 
-app.get('/get/:item', (req, res) => {
-  res.send(`Here you have "${req.params.item}", with query = ${JSON.stringify(req.query)}`)
+app.get('/path/to/:item', (req, res) => {
+  res.send(`You requested "${req.params.item}" to server with query = ${JSON.stringify(req.query)}`)
+})
+
+app.post('/userInput', (req, res) => {
+  res.send({ message: `We have taken note of your input, "${req.body.input}", thanks` })
 })
 
 const port = 3000
